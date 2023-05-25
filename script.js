27e8ba9ecb1da1bc28cd3e8cc9dbceb41fe6ac60
sattara1985 <sattara1985@gmail.com>
2023-05-24 20:49:13 -0500

    correcto

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-
-
 document.write("Hola mundo X2");
-
-
 document.write('<h1 style="color:red">Bienvenidos a el repaso de JS</h1>');
-
 console.log("Hola mundo!");
 console.warn("mensajes en amarillo");
 console.error("mensaje error en rojo");
 //lenguaje interpretado es javascript
 alert("Hola mundo 3");
-
 let nombre=""; // si no se pone valor queda en undefined si se deja en null no sabe si va ser tipo string 
 console.log(`El valor de nombre antes del prompt: ${nombre}.`);
 nombre = prompt("Ingresa tu nombre");
